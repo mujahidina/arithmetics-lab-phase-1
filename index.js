49d338196d726080cbdac33f71ffd41866875464
mujahidina <mujahidabdi5070@gmail.com>
2023-12-12 15:14:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
+  if  (distance <= 400){
+    return "This one is on me!";
+  } else if (distance > 2000 && distance < 2500){
+   return   "I will gladly take your thirty bucks.";
+  } else if (distance > 2500) {
+    return "No can do.";
+  }
+   
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+   return  city === "NYC" ?  "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case `generous`:
+    return 'Thank you so much.'
+
+
+    case `not as generous`:
+        return `Thank you.`
+
+
+        default:
+          return `Bye.`
+
+  }
 }
